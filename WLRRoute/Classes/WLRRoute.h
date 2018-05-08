5a49289b8c03d1e59edd2b8bc30ad338559cbe3f
wudonghui <donghui.wu@zhaopin.com.cn>
2018-05-08 14:06:37 +0800

    增加头文件引用

--- WLRRoute/Classes/WLRRoute.h
+++ WLRRoute/Classes/WLRRoute.h
@@ -20,4 +20,17 @@
 #import <WLRRoute/WLRRouter.h>
 #import <WLRRoute/WLRRouteRequest.h>
 */
+
+#import "NSError+WLRError.h"
+#import "NSString+WLRQuery.h"
+#import "UIViewController+WLRRoute.h"
+#import "WLRMatchResult.h"
+#import "WLRRegularExpression.h"
+#import "WLRRouteHandler.h"
+#import "WLRRouteMatcher.h"
+#import "WLRRouteMiddlewareProtocol.h"
+#import "WLRRouter.h"
+#import "WLRRouteRequest.h"
+
+
 #endif /* WLRRoute_h */
